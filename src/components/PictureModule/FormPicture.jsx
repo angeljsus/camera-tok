7cca64daf5fc38e0e6a56eb90b9f7aca036cf3eb
AGUILAR TRUJILLO ANGEL DE JESUS <ANGEL.TRUJILLO@inegi.org.mx>
2023-07-05 08:27:30 -0600

    Evento conectar y desconectar dispositivo

--- src/components/PictureModule/FormPicture.jsx
+++ src/components/PictureModule/FormPicture.jsx
@@ -8,19 +8,31 @@ import './FormPicture.css';
 
 const useDevices = () => {
 	const [arrayDevices, setArrayDevices] = useState([]);
-	const {
-		_capturedState, _setStreamingObj
-	} = useContext(Context);
+	const { _capturedState, _setStreamingObj, _videoElement } = useContext(Context);
 
 	useEffect(() => {
 		console.log('#1 getting and setting devices...')
-		getArrayDevices()
-			.then(result => {
-				console.log(result)
-				result.length ? _setStreamingObj(result[0]) : false;
-				setArrayDevices(result)
-			})
+    navigator.mediaDevices.addEventListener('devicechange', updateSelect);
+		updateDevicesData();
+    
+    return () => {
+      navigator.mediaDevices.removeEventListener('devicechange', updateSelect);
+    };
 	}, []);
+
+  const updateSelect = () => {
+    _videoElement.current.src = null;
+    _setStreamingObj({});
+    return updateDevicesData();
+  };
+
+  const updateDevicesData = () => {
+    return getArrayDevices().then((result) => {
+      console.log(result);
+      result.length ? _setStreamingObj(result[0]) : false;
+      setArrayDevices(result);
+    });
+  }
 
 	// return an array of devices
 	const getArrayDevices = () => {
